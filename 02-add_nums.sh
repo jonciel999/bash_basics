d59325e83c927336201d889d7c9917ec1fe39420
Jonathan Roof <jwroofsound@gmail.com>
2020-08-30 17:44:34 -0400

    This is a test of branching

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,9 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter a number for width"
+read width
+echo "Enter a number for height"
+read height
+pixels=$((width*height))
+echo "The number of pixels is $pixels"
